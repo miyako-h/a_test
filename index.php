c664312b3896d127b8d343c45c133e6049268827
myk <98862036+miyako-h@users.noreply.github.com>
2022-03-11 11:27:07 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>アナリティクステストページ</title>
+</head>
+<body>
+<div>
+	<h1>Hello New Word!</h1>
+	<p>GitHubにテストアップしています。</p>
+</div>
+</body>
+</html>
